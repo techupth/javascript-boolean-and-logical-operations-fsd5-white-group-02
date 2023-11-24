91de4beca88ba3aa99753f0251b23f81eb27e1b9
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-11-24 16:00:55 +0700

    fix bug

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,20 @@
-let isOver18 = true;
-let hasCriminalBlacklist = true;
+let isOver18;
+let hasCriminalBlacklist;
 
-let isAllow = true;
+let isAllow;
 
-// Start coding here
+// ให้ Assign ค่าให้กับ ตามของคุณสมบัติของ James
+// James มีอายุ 18 ปี
+// James ไม่เคยมีประวัติอาชญากรรม
 
-isAllow = isOver18 && hasCriminalBlacklist;
+isOver18 = true;
+hasCriminalBlacklist = false;
+
+// ให้ Reassign ค่า ตามเงื่อนไขข้างต้น
+// เงื่อนไขในการเข้าร่วมก็คือ
+// ผู้เข้าร่วมงานต้องมีอายุมากกว่า 18 ปีขึ้นไป
+// และต้องไม่เคยมีประวัติอาชญากรรมมาก่อน
+
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,18 +1,35 @@
 // Exercise #2: Promotion Conditions
 // Start coding here
-let lastMonthPaidMoreThan4000 = true;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = true;
-let hasAttendedDiscountEvent = true;
-let isPlatinum = true;
+let lastMonthPaidMoreThan4000;
+let isWeekday;
+let hasBoughtProductFromITCategory;
+let hasAttendedDiscountEvent;
+let isPlatinum;
 
-let hasPromotion = true;
+let hasPromotion;
+
+//ให้ Assign ตัว Value ตามของคุณสมบัติของ John
+// มียอดสั่งซื้อเดือนที่แล้ว 4001 บาท
+// ไปซื้อของวันศุกร์
+// ไม่เคยซื้อของในหมวดหมู่ IT มาก่อน
+// เคยเข้าร่วมกิจกรรมมหกรรมลดราคาครั้งที่ 2
+// เป็น Member ระดับ Gold
+
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+
+//ให้ Reassign ค่า เป็น Expression ของ Variable อื่นๆ ตามเงื่อนไขข้างต้น
+//กรณีที่ 1: ผู้ซื้อมียอดสั่งซื้อสินค้าจากเดือนที่แล้วมากกว่า 4000 บาท และวันที่ซื้อสินค้าเป็นวันจันทร์ถึงวันศุกร์ และไม่เคยซื้อสินค้าจากหมวดหมู่ IT มาก่อน และไม่เคยเข้าร่วมกิจกรรมมหกรรมลดราคามาก่อน
+//กรณีที่ 2 : ผู้ซื้อเป็น Member ในระดับ Platinum"
 
 hasPromotion =
   (lastMonthPaidMoreThan4000 &&
     isWeekday &&
-    !hasBoughtProductFromITCategory &&
-    !hasAttendedDiscountEvent) ||
-  !isPlatinum;
+    hasBoughtProductFromITCategory &&
+    hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
