a549b6b99a67f7ae415c28b75425d0f3f0e4776f
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-11-24 13:23:12 +0700

    Modify : Fix bugs and Reassign Variable

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,7 @@
 //Exercise #3: Debugging (Boolean & Logical Operations)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
-
-numberA = 10;
+const numberA = 10;
+const numberB = 20;
 
 console.log(`Sum: ${numberA + numberB}`);
